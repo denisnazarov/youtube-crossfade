ab1fb7572759ac014ca8322fe941b868038d4af9
Denis Nazarov <zvezda@gmail.com>
2014-09-07 21:40:26 -0400

    load videos from yt playlist

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,31 +1,21 @@
+import Ember from 'ember';
+import ajax from 'ic-ajax';
+
 export default Ember.Route.extend({
   model: function(){
-    return [
-      {
-        videoId: 'voUVDAgFtho',
-        startTime: 18,
-        endTime: 23
-      },
-      {
-        videoId: 'eDmEnyORhA4',
-        startTime: 18,
-        endTime: 23
-      },
-      {
-        videoId: 'pukbzCC3cfQ',
-        startTime: 15,
-        endTime: 20
-      },
-      {
-        videoId: 'lkqtQ61k58E',
-        startTime: 10,
-        endTime: 15
-      },
-      {
-        videoId: 'IEgKSc_V71Y',
-        startTime: 10,
-        endTime: 15
-      }
-    ]
+    var playlistURL = "http://gdata.youtube.com/feeds/api/playlists/PLArvEia7B_Fyn_GeKqv4s1iBwuYL1Mem-?v=2&alt=json&start-index=60&max-results=50";
+    return ajax(playlistURL).then(function(data){
+      return data.feed.entry.map(function(entry){
+        var aspectRatio = entry.media$group.yt$aspectRatio && entry.media$group.yt$aspectRatio.$t || 'fullscreen';
+        var duration = entry.media$group.yt$duration.seconds;
+        var videoID = entry.media$group.yt$videoid.$t;
+        return {
+          aspectRatio: aspectRatio,
+          videoId: videoID,
+          startTime: 0,
+          endTime: 15
+        };
+      });
+    });
   }
 });
